c3e2e3386aed9aa01dcfc605e2f6413bccf09539
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-26 20:08:19 -0700

    Update Number-order.js

--- Number-order.js
+++ Number-order.js
@@ -18,3 +18,6 @@ For example:
   inAscOrder([1,6,10,18,2,4,20]); // returns false
   inAscOrder([9,8,7,6,5,4,3,2,1]); // returns false because the numbers are in DESCENDING order
 */
+
+
+// Solution
