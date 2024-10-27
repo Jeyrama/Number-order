50d7438cac1bb099fabb5ebe367e44852fd9c03d
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-26 20:08:42 -0700

    Update Number-order.js

--- Number-order.js
+++ Number-order.js
@@ -21,3 +21,12 @@ For example:
 
 
 // Solution
+
+function inAscOrder(arr) {
+  for(let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i+1]) {
+      return false; 
+    }
+  }
+  return true;
+}
